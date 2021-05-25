f86a8ec06513beb246286237ff7579549cf9d423
Vos, Jani de <jani.devos@wur.nl>
2021-05-25 11:00:57 +0200

    final

--- CGMap_ToMethylKit.py
+++ CGMap_ToMethylKit.py
@@ -11,13 +11,13 @@ with gzip.open(sys.argv[1],'rb') as CGmap:
                 strand="F"
             else:
                 strand="R"
-                perc_C=float(perc_C)*100
-                perc_T=100-(perc_C)
-                outlist.append(str("chr"+chr+"."+pos))
-                outlist.append(str("chr"+chr))
-                outlist.append(str(pos))
-                outlist.append(strand)
-                outlist.append(str(cov))
-                outlist.append(str(perc_C))
-                outlist.append(str(perc_T))
-                print("\t".join(outlist))
+            perc_C=float(perc_C)*100
+            perc_T=100-(perc_C)
+            outlist.append(str("chr"+chr+"."+pos))
+            outlist.append(str("chr"+chr))
+            outlist.append(str(pos))
+            outlist.append(strand)
+            outlist.append(str(cov))
+            outlist.append(str(perc_C))
+            outlist.append(str(perc_T))
+            print("\t".join(outlist))
